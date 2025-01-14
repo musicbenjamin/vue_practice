e411a9aea457b4f51a84385711756ad1439a46b3
musicbenjamin <musicbenjamin01@gmail.com>
2025-01-14 15:42:16 +0100

    count app

---
+++ my-vue-app4/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Counter />
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+
+export default {
+  name: "App",
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-vue-app4/src/components/Counter.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="counter-app">
+    <h1>Counter Component</h1>
+    <p>Original Count: {{ count }}</p>
+    <p>Computed Value (Count x 2): {{ doubleCount }}</p>
+    <button @click="decrement">Decrement</button>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script>
+import { ref, computed } from "vue";
+
+export default {
+  name: "Counter",
+  setup() {
+    const count = ref(0);
+
+    const doubleCount = computed(() => count.value * 2);
+
+    const increment = () => {
+      count.value++;
+    };
+
+    const decrement = () => {
+      count.value--;
+    };
+
+    return {
+      count,
+      doubleCount,
+      increment,
+      decrement,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.counter-app {
+  text-align: center;
+  margin-top: 50px;
+}
+
+button {
+  margin: 5px;
+  padding: 10px 20px;
+  font-size: 16px;
+}
+</style>
