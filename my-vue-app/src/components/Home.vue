548d72b650fa1747d16e9c912095a54e22f87300
musicbenjamin <musicbenjamin01@gmail.com>
2025-01-15 17:00:13 +0100

    API

---
+++ my-vue-app/src/components/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <button @click="fetchRandomQuote">Get Random Quote</button>
+    <QuoteDisplay :quote="quote"/>
+  </div>
+</template>
+
+<script>
+
+import QuoteDisplay from './QuoteDisplay.vue';
+
+export default {
+  components: {
+    QuoteDisplay
+  },
+  data() {
+    return {
+      quote: null
+    };
+  },
+  methods: {
+    async fetchRandomQuote() {
+      console.log('Fetching quote...');
+      try {
+        const response = await fetch('https://api.kanye.rest/');
+        const data = await response.json();
+        this.quote = data.content;
+        console.log('Quote fetched:', data.quote);
+        this.quote = data.quote;
+      } catch (error) {
+        console.error("Error fetching quote: ", error);
+      }
+    }
+  }
+}
+
+</script>
---
+++ my-vue-app/src/components/QuoteDisplay.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-if="quote">
+    <p>{{ quote }}</p>
+  </div>
+  <div v-else>
+    <p>Loading...</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    quote: {
+      type: String,
+      default: null
+    }
+  }
+};
+</script>
