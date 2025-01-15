6f3c81954fb220294e035363a8d075397515a241
musicbenjamin <musicbenjamin01@gmail.com>
2025-01-15 09:52:14 +0100

    router

---
+++ my-vue-app6/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <nav>
+    <router-link to ="/">Home</router-link>
+    <router-link to ="/about">About</router-link>
+  </nav>
+  <router-view/>
+</div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  nav {
+  background: #f8f9fa;
+  padding: 1rem;
+}
+nav a {
+  margin-right: 10px;
+  text-decoration: none;
+  color: #007bff;
+}
+nav a:hover {
+  text-decoration: underline;
+}
+</style>
